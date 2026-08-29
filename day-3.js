a6b065ac87e6465a095453a7aa3550ae1fdd7c68
Matthew Wainwright <mwwainwright1014@gmail.com>
2026-08-29 13:41:34 -0500

    second day 3 button describes a different cat, closes #5

--- day-3.js
+++ day-3.js
@@ -1,5 +1,6 @@
 let button = document.getElementById("describe-button");
 let output = document.getElementById("output");
+let secondButton = document.getElementById("describe-second-button");
 
 function showPet(pet) {
     output.textContent = pet.name + " comes from " + pet.origin + ".";
@@ -7,4 +8,8 @@ function showPet(pet) {
 
 button.addEventListener("click", function () {
     showPet(pets[0]);
+});
+
+secondButton.addEventListener("click", function() {
+    showPet(pets[1]);
 });
